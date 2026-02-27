da2f5d88c3ac46b283e1fe4b0507b7e718308012
mkuehbach <markus.kuehbach@physik.hu-berlin.de>
2026-02-27 11:28:16 +0100

    final touches

--- src/nomad_north_apmtools/north_tools/apmtools/__init__.py
+++ src/nomad_north_apmtools/north_tools/apmtools/__init__.py
@@ -19,7 +19,7 @@ from nomad.config.models.north import NORTHTool
 from nomad.config.models.plugins import NorthToolEntryPoint
 
 apmtools = NORTHTool(
-    image='ghcr.io/fairmat-nfdi/nomad-north-apmtools:latest',
+    image='ghcr.io/fairmat-nfdi/nomad-north-apmtools:main',
     description="""### **apmtools**:
 
     [paraprobe-toolbox](https://gitlab.com/paraprobe/paraprobe-toolbox)
@@ -28,9 +28,7 @@ apmtools = NORTHTool(
 
     [compositionspace](https://github.com/eisenforschung/CompositionSpace)
 
-    [APyT](https://github.com/sebi-85/apyt)
-
-    [APTyzer](https://github.com/areichm/APTyzer)""",
+    [APyT](https://github.com/sebi-85/apyt)""",
     short_description='Jupyterlab with domain-specific open-source software for atom probe research',
     external_mounts=[],
     file_extensions=['nxs, pos, epos, rng, rrng, apt, ato, raw, h5, hdf5, ipynb'],
@@ -45,6 +43,7 @@ apmtools = NORTHTool(
     display_name='apmtools',
 )
 
+#    [APTyzer](https://github.com/areichm/APTyzer)
 #    [pyccapt](https://github.com/mehrpad/pyccapt)
 
 north_tool_entry_point = NorthToolEntryPoint(
